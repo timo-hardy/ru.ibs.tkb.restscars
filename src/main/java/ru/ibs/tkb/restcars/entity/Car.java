661e49d9fb324cbc47c3cb1d9568a9b8cb32b90a
Timur <timos.gooogle@gmail.com>
2021-11-11 16:35:14 +0500

    Added @Entity in Car.class

--- src/main/java/ru/ibs/tkb/restcars/entity/Car.java
+++ src/main/java/ru/ibs/tkb/restcars/entity/Car.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
@@ -11,7 +12,7 @@ import javax.persistence.Id;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Entity
 public class Car {
     @Id
     @GeneratedValue
