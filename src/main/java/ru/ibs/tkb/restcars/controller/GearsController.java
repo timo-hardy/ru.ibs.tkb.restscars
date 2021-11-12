6adcb5fb65cf51d69ba1bc58fd3996c610a959ee
Timur <timos.gooogle@gmail.com>
2021-11-12 13:23:15 +0500

    Implemented PistonsController

---
+++ src/main/java/ru/ibs/tkb/restcars/controller/GearsController.java
@@ -0,0 +1,36 @@
+package ru.ibs.tkb.restcars.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.ibs.tkb.restcars.entity.Engine;
+import ru.ibs.tkb.restcars.entity.Gears;
+import ru.ibs.tkb.restcars.service.EngineService;
+import ru.ibs.tkb.restcars.service.GearsService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/gears")
+public class GearsController {
+    private final GearsService gearsService;
+
+    @Autowired
+    public GearsController(GearsService gearsService) {
+        this.gearsService = gearsService;
+    }
+
+    @GetMapping("/read")
+    public List<Gears> getAll () {
+        return  gearsService.findAll();
+    }
+
+    @RequestMapping("/create")
+    public void add(@RequestBody Gears gears) {
+        gearsService.add(gears);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@PathVariable long id) {
+        gearsService.delete(id);
+    }
+}
---
+++ src/main/java/ru/ibs/tkb/restcars/controller/PistonsController.java
@@ -0,0 +1,36 @@
+package ru.ibs.tkb.restcars.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.stylesheets.LinkStyle;
+import ru.ibs.tkb.restcars.entity.Pistons;
+import ru.ibs.tkb.restcars.service.PistonsService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pisotons")
+public class PistonsController {
+    private final PistonsService pistonsService;
+
+    @Autowired
+    public PistonsController(PistonsService pistonsService) {
+        this.pistonsService = pistonsService;
+    }
+
+    @GetMapping("/read")
+    public List<Pistons> getAll() {
+        return pistonsService.findAll();
+    }
+
+    @RequestMapping("/create")
+    public void add(@RequestBody Pistons pistons) {
+        pistonsService.add(pistons);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@PathVariable long id){
+        pistonsService.delete(id);
+    }
+}
+
