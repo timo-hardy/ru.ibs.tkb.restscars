3463d1acd4ee88c038a0a8412b08cb8dd69a437e
Timur <timos.gooogle@gmail.com>
2021-11-12 13:05:31 +0500

    Implemented CarController

--- src/main/java/ru/ibs/tkb/restcars/controller/CarController.java
+++ src/main/java/ru/ibs/tkb/restcars/controller/CarController.java
@@ -1,10 +1,35 @@
 package ru.ibs.tkb.restcars.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.ibs.tkb.restcars.entity.Car;
+import ru.ibs.tkb.restcars.service.CarService;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/car")
 public class CarController {
+    private final CarService carService;
 
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/read")
+    public List<Car> getAll () {
+        return  carService.findAll();
+    }
+
+    @RequestMapping("/create")
+    public void add(@RequestBody Car car) {
+        carService.add(car);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@PathVariable long id) {
+        carService.delete(id);
+    }
 }
