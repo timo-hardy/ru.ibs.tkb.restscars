45a411bfe75703081cc089800a7ed9eefa87cc81
Timur <timos.gooogle@gmail.com>
2021-11-12 13:08:58 +0500

    Implemented EngineController

---
+++ src/main/java/ru/ibs/tkb/restcars/controller/EnigneController.java
@@ -0,0 +1,36 @@
+package ru.ibs.tkb.restcars.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.ibs.tkb.restcars.entity.Car;
+import ru.ibs.tkb.restcars.entity.Engine;
+import ru.ibs.tkb.restcars.service.CarService;
+import ru.ibs.tkb.restcars.service.EngineService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/engine")
+public class EnigneController {
+    private final EngineService engineService;
+
+    @Autowired
+    public EnigneController(EngineService engineService) {
+        this.engineService = engineService;
+    }
+
+    @GetMapping("/read")
+    public List<Engine> getAll () {
+        return  engineService.findAll();
+    }
+
+    @RequestMapping("/create")
+    public void add(@RequestBody Engine engine) {
+        engineService.add(engine);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@PathVariable long id) {
+        engineService.delete(id);
+    }
+}
